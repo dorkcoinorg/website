9e1ee53637964cc8c66d043be2ff959ba8e696f2
Dorky <chriseverhold@gmail.com>
2026-04-18 13:52:13 +0100

    Update exchanges.jsx

--- components/exchanges/exchanges.jsx
+++ components/exchanges/exchanges.jsx
@@ -32,6 +32,15 @@ const Exchanges = () => {
               <span className="ff-1">Cexius</span>
             </Link>
           </div>
+          <div className="col-6 col-md-4">
+            <Link
+              href="https://cexius.com/trade/DORK-USDT"
+              className="btn btn-lg btn-light text-heading w-100 d-flex justify-content-center align-items-center gap-2 rounded gsap-fade-in"
+            >
+              <span className="ff-1">https://trade.nestex.one/spot/dork</span>
+            </Link>
+          </div>
+          
 
           {/* FUTURE */}
           <div className="col-6 col-md-4">
