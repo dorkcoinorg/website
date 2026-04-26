1a1390e219016be09efde09aa1971e3b7aaf4641
Dorky <chriseverhold@gmail.com>
2026-04-26 14:55:55 +0100

    Update exchanges.jsx

--- components/exchanges/exchanges.jsx
+++ components/exchanges/exchanges.jsx
@@ -23,21 +23,29 @@ const Exchanges = () => {
         {/* BUTTONS */}
         <div className="row g-4 justify-content-center">
 
-          {/* Cexius */}
-          <div className="col-6 col-md-4">
-            <Link
-              href="https://cexius.com/trade/DORK-USDT"
-              className="btn btn-lg btn-light text-heading w-100 d-flex justify-content-center align-items-center gap-2 rounded gsap-fade-in"
-            >
-              <span className="ff-1">Cexius</span>
-            </Link>
-          </div>
+          
           <div className="col-6 col-md-4">
             <Link
               href="https://trade.nestex.one/spot/dork"
               className="btn btn-lg btn-light text-heading w-100 d-flex justify-content-center align-items-center gap-2 rounded gsap-fade-in"
             >
               <span className="ff-1">NestEx</span>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link
+              href="https://klingex.io/trade/DORK-USDT"
+              className="btn btn-lg btn-light text-heading w-100 d-flex justify-content-center align-items-center gap-2 rounded gsap-fade-in"
+            >
+              <span className="ff-1">KlingEx</span>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link
+              href="https://cexius.com/trade/DORK-USDT"
+              className="btn btn-lg btn-light text-heading w-100 d-flex justify-content-center align-items-center gap-2 rounded gsap-fade-in"
+            >
+              <span className="ff-1">Cexius</span>
             </Link>
           </div>
           
